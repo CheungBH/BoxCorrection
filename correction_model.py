6b3fda8fe64387bb3b2ca5dea3c1e2c577cfc782
cheungbh <534660436@qq.com>
2021-10-20 16:51:06 +0800

    add correction model

--- correction_model.py
+++ correction_model.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-cls_dim = 128
+cls_dim = 16
 box_dim = 32
 
 
@@ -11,26 +11,26 @@ class CorrectionNet(nn.Module):
         self.n_classes = class_num
         self.cls_embedding = nn.Linear(self.n_classes+1, cls_dim)
         self.box_embedding = nn.Linear(4, box_dim)
-        self.image_project = nn.Conv2d()
-        self.image_pooling = nn.AdaptiveAvgPool2d()
-        self.instance_project = nn.Conv2d()
-        self.instance_pooling = nn.AdaptiveAvgPool2d()
+        self.image_project = nn.Conv2d(512, 64, kernel_size=3, stride=1, padding=3, bias=False)
+        self.image_pooling = nn.AdaptiveAvgPool2d(1)
+        self.instance_project = nn.Conv2d(512, 64, kernel_size=3, stride=1, padding=3, bias=False)
+        self.instance_pooling = nn.AdaptiveAvgPool2d(1)
         self.hdim = 128
 
         self.forward_net = nn.Sequential(
-            nn.Linear(in_dim, self.hdim),
+            nn.Linear(176, self.hdim),
             nn.Tanh(),
             nn.Linear(self.hdim, self.hdim),
             nn.Tanh(),
-            nn.Linear(self.hdim, class_num)
+            nn.Linear(self.hdim, class_num+5)
         )
 
     def forward(self, image_feature, instance_feature, class_possibility, box_reg):
         cls = self.cls_embedding(class_possibility)
         box = self.box_embedding(box_reg)
 
-        img = self.image_pooling(self.image_project(image_feature)).flatten()
-        instance = self.instance_pooling(self.instance_project(instance_feature)).flatten()
+        img = self.image_pooling(self.image_project(image_feature)).view(1,-1)
+        instance = self.instance_pooling(self.instance_project(instance_feature)).view(1,-1)
 
         feat = torch.cat([img, instance, cls, box], dim=-1)
 
@@ -43,5 +43,6 @@ if __name__ == '__main__':
     folder = "h5"
     boxes_label, cls_label, image_feature, instance_feature, cls_preds, boxes_preds = CorrectionDataset(folder)[0]
     net = CorrectionNet(class_num=1)
-    output = net(image_feature, instance_feature, cls_preds, boxes_preds)
+    output = net(image_feature, instance_feature[0].unsqueeze(dim=0), cls_preds[0].unsqueeze(dim=0), boxes_preds[0].unsqueeze(dim=0))
+    print(output)
 
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from correction_loader import Dataloader
 epochs = 20
 dataset_path = "h5"
 
-net = CorrectionNet()
+net = CorrectionNet(1)
 loader = Dataloader(dataset_path).build_loader()
 
 
 for epoch in range(epochs):
     for i, data in enumerate(loader):
         boxes_label, cls_label, image_feature, instance_feature, cls_preds, boxes_preds = data
+        output = net()
 
 
 
