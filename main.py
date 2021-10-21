8ab0e90d496c09b44388d28503ab9cd21c33e8b5
cheungbh <534660436@qq.com>
2021-10-21 17:02:22 +0800

    add optimizer and mix precision

--- main.py
+++ main.py
@@ -2,15 +2,25 @@
 from correction_model import CorrectionNet
 from correction_loader import Dataloader
 import torch.nn.functional as F
+import torch.optim as optim
+
+try:
+    from apex import amp
+    mix_precision = True
+except:
+    mix_precision = False
 
 epochs = 1
 dataset_path = "h5"
+LR = 0.001
 device = "cpu"
 
 cls_crit = F.cross_entropy
 
 net = CorrectionNet(1)
 loader = Dataloader(dataset_path).build_loader(batch_size=2, num_worker=0)
+optimizer = optim.Adam(net.parameters(), lr=LR)
+
 
 if device != "cpu":
     net.cuda()
@@ -20,8 +30,15 @@ for epoch in range(epochs):
     for i, data in enumerate(loader):
         boxes_label, cls_label, image_feature, instance_feature, boxes_preds, cls_preds = data
         output = net(image_feature, instance_feature, cls_preds, boxes_preds)
-        # print(output)
-        crit_loss = cls_crit(output[:, :2], cls_label)
+        crit_loss = cls_crit(output[:, :2], cls_label.long())
         print(crit_loss)
 
+        loss = crit_loss
+
+        if mix_precision:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+        optimizer.step()
 
