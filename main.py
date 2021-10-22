d983e086f206c614e9c4b253deb26340da04d94f
cheungbh <534660436@qq.com>
2021-10-22 15:55:01 +0800

    add box regression loss

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch
 import os
+from utils import generate_box_weight, _smooth_l1_loss
 
 try:
     from apex import amp
@@ -35,10 +36,13 @@ for epoch in range(epochs):
     for i, data in enumerate(loader):
         boxes_label, cls_label, image_feature, instance_feature, boxes_preds, cls_preds = data
         output = net(image_feature, instance_feature, cls_preds, boxes_preds)
-        crit_loss = cls_crit(output[:, :2], cls_label.long())
+        cls_loss = cls_crit(output[:, :2], cls_label.long())
+        b_weight = generate_box_weight(cls_label)
+        reg_loss = _smooth_l1_loss(output[:, 2:], boxes_label, b_weight, b_weight)
         # print(crit_loss)
 
-        loss = crit_loss
+        loss = cls_loss + reg_loss
+        print(loss)
 
         if mix_precision:
             with amp.scale_loss(loss, optimizer) as scaled_loss:
