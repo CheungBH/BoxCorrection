a6c482546ee7e3d94b81686402ae5312f728554c
cheungbh <534660436@qq.com>
2021-11-05 10:27:41 +0800

    add logger for error analysis

--- error_analysis.py
+++ error_analysis.py
@@ -7,33 +7,25 @@ import os
 from utils import generate_box_weight, _smooth_l1_loss
 import time
 from opt import opt
+from logger import ErrorAnalysisLogger
 
 dataset_path = opt.dataset_root
 device = opt.device
-model_dir = os.path.join("weights", opt.expFolder, opt.expID,)
-optimize = opt.optMethod
-os.makedirs(model_dir, exist_ok=True)
-
+model_path = ""
+logger = ErrorAnalysisLogger("/".join(model_path.split("/")[:-1]))
 cls_crit = F.cross_entropy
 
 dataset = Dataloader(dataset_path)
 loader = dataset.build_loader(batch_size=1, num_worker=opt.num_worker)
 net = CorrectionNet(dataset.num_class)
+net.load_state_dict(torch.load(model_path))
 if device != "cpu":
     net.cuda()
 
-log = open(os.path.join(model_dir, "log.txt"), "w")
-best_loss = float("inf")
-
 net.eval()
-
-loss_sum = torch.zeros(1)
 errors = []
-if device != "cpu":
-    loss_sum = loss_sum.cuda()
 
 for i, data in enumerate(loader):
-    opt.iterations += 1
     boxes_label, cls_label, image_feature, instance_feature, boxes_preds, cls_preds, image_name = data
     if device != "cpu":
         output = net(image_feature.cuda(), instance_feature.cuda(), cls_preds.cuda(), boxes_preds.cuda()).cpu()
@@ -47,7 +39,6 @@ for i, data in enumerate(loader):
     in_reg_loss = _smooth_l1_loss(boxes_preds, boxes_label, b_weight, b_weight)
 
     fg = torch.argmax(cls_label[0]).tolist()
-    errors.append([image_name, fg, in_cls_loss, in_reg_loss, out_cls_loss, out_reg_loss])
+    errors.append([image_name[0], fg, in_cls_loss.tolist(), in_reg_loss.tolist(), out_cls_loss.tolist(), out_reg_loss.tolist()])
 
-print(errors)
 
--- logger.py
+++ logger.py
@@ -17,3 +17,27 @@ class BatchLogger:
 
     def write_results(self, bs, opti, lr, epo, br, loss, sched, mom):
         self.file.write("{},{},{},{},{},{},{},{},{}\n".format(self.model_idx, bs, opti, lr, epo, br, sched, mom, loss))
+
+
+class ErrorAnalysisLogger:
+    def __init__(self, folder):
+        excel_path = os.path.join("/".join(folder.split("/")[:-1]), "error_analysis.csv")
+        if not os.path.exists(excel_path):
+            self.file = open(excel_path, "w")
+            self.write_title()
+        else:
+            self.file = open(excel_path, "a+")
+        self.model_idx = folder.split("/")[-1]
+
+    def write_title(self):
+        title = "image name, objectness, output cls-loss, output reg-loss, input cls-loss, input reg-loss\n"
+        self.file.write(title)
+
+    def write_results(self, item):
+        im_name, fg, out_cls, out_reg, in_cls, in_reg = item
+        self.file.write("{},{},{},{},{},{}\n".format(im_name, fg, out_cls, out_reg, in_cls, in_reg))
+
+    def write_summarize(self, ls):
+        for item in ls:
+            self.write_results(item)
+
