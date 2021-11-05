31f041c074bf65b41da2c34bc9a97fdde5454999
cheungbh <534660436@qq.com>
2021-11-05 10:48:34 +0800

    add logger for error analyser

--- error_analysis.py
+++ error_analysis.py
@@ -11,14 +11,15 @@ from logger import ErrorAnalysisLogger
 
 dataset_path = opt.dataset_root
 device = opt.device
-model_path = ""
-logger = ErrorAnalysisLogger("/".join(model_path.split("/")[:-1]))
+# model_path = opt.loadModel
+model_path = "weights/best_27_new.pth"
+logger = ErrorAnalysisLogger(model_path)
 cls_crit = F.cross_entropy
 
 dataset = Dataloader(dataset_path)
 loader = dataset.build_loader(batch_size=1, num_worker=opt.num_worker)
 net = CorrectionNet(dataset.num_class)
-net.load_state_dict(torch.load(model_path))
+net.load_state_dict(torch.load(model_path, map_location=opt.device))
 if device != "cpu":
     net.cuda()
 
@@ -38,7 +39,7 @@ for i, data in enumerate(loader):
     in_cls_loss = cls_crit(cls_preds, cls_label.long())
     in_reg_loss = _smooth_l1_loss(boxes_preds, boxes_label, b_weight, b_weight)
 
-    fg = torch.argmax(cls_label[0]).tolist()
-    errors.append([image_name[0], fg, in_cls_loss.tolist(), in_reg_loss.tolist(), out_cls_loss.tolist(), out_reg_loss.tolist()])
+    # fg = torch.argmax(cls_label[0]).tolist()
+    errors.append([image_name[0], cls_label[0].tolist(), in_cls_loss.tolist(), in_reg_loss.tolist(), out_cls_loss.tolist(), out_reg_loss.tolist()])
 
-
+logger.write_summarize(errors)
